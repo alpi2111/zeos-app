cca49191ad97fba109178afa850909e66a2d1c83
alpi2111 <carlos.alpi21@gmail.com>
2019-11-07 03:12:24 -0600

    Se agrego el bottom navigation bar al encargadgo, no hace nada aún

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -18,6 +18,7 @@ class _HomePageState extends State<HomePage> {
   final _scaffoldKeyHome = GlobalKey<ScaffoldState>();
   final _sucursalProvider = new SucursalProvider();
   final _herramientaProvider = new HerramientaProvider();
+  int _index = 0;
 
   //final con = new Con();
 
@@ -28,6 +29,27 @@ class _HomePageState extends State<HomePage> {
     //ConnectivityUtils.instance.setCallback((response) => response.contains('other'));
     //ConnectivityUtils.instance.setServerToPing('https://google.com.mx');
     return Scaffold(
+        bottomNavigationBar: _prefs.isAdmin
+            ? null
+            : BottomNavigationBar(
+                currentIndex: _index,
+                onTap: (index) {
+                  setState(() {
+                    _index = index;
+                  });
+                },
+                items: [
+                  BottomNavigationBarItem(
+                      icon: RotatedBox(
+                        child: Icon(Icons.system_update_alt),
+                        quarterTurns: 2,
+                      ),
+                      title: Text('Préstamos')),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.system_update_alt),
+                      title: Text('Devoluciones'))
+                ],
+              ),
         floatingActionButton: _prefs.isAdmin ? _fabAdmin() : _fabNoAdmin(),
         persistentFooterButtons: _prefs.isAdmin ? _pbtnAdmin() : _pbtnNoAdmin(),
         key: _scaffoldKeyHome,
@@ -57,8 +79,8 @@ class _HomePageState extends State<HomePage> {
             builder: _prefs.isAdmin
                 ? (context, isOnline) =>
                     _crearBtnSucursales(isOnline) //es el supervisor
-                : (context, isOnline) =>
-                    _crearHerramientasSucursal(isOnline) //no es el supervisor
+                : (context, isOnline) => _crearHerramientasSucursal(
+                    isOnline, _index) //no es el supervisor
             ) /*: Center(
                                                 child: Text('Sin Conexion'),
                                               ),*/
@@ -157,14 +179,27 @@ class _HomePageState extends State<HomePage> {
   }
 
   //TODO hacer la parte del encargado sin admin
-  Widget _crearHerramientasSucursal(bool isOnline) {
-    return FutureBuilder(
-      future: _herramientaProvider.obtenerHerramientasSucursal('j'),
-      //initialData: InitialData,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return Container();
-      },
-    );
+  Widget _crearHerramientasSucursal(bool isOnline, int index) {
+    switch (index) {
+      case 0:
+        return FutureBuilder(
+          future: _herramientaProvider.obtenerHerramientasSucursal('j'),
+          //initialData: InitialData,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return Container();
+          },
+        );
+      case 1:
+        return Container(
+          color: Colors.red,
+        );
+
+        break;
+      default:
+        return Center(
+          child: Text('Pagina no encontrada'),
+        );
+    }
   }
 
   /*_cerrarSesion(bool isOn) async {
@@ -205,7 +240,8 @@ class _HomePageState extends State<HomePage> {
     return FloatingActionButton(
       heroTag: 'btnCerrarSesion',
       onPressed: () async {
-        bool cerrar = await mostrarAlertaCerrarSesion(context, 'Cerrar Sesión', '¿Desea Cerrar Sesión?');
+        bool cerrar = await mostrarAlertaCerrarSesion(
+            context, 'Cerrar Sesión', '¿Desea Cerrar Sesión?');
         if (cerrar) {
           _prefs.token = null;
           Navigator.of(context).pushReplacementNamed('login');
@@ -239,7 +275,10 @@ class _HomePageState extends State<HomePage> {
     return <Widget>[
       RaisedButton(
         onPressed: () {},
-        child: Text('Agregar Herramienta', style: TextStyle(color: Colors.white),),
+        child: Text(
+          'Agregar Herramienta',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     ];
   }
